df8c4c01c67a446f71ab6b143101c9c1e34b2ed7
klaoha06 <laohasongkram.supakorn@gmail.com>
2015-07-21 17:45:29 +0700

    add links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def soundcloud_user
+			$sc_consumer.get('/me')
+	end
+	def soundcloud_tracks
+			$sc_consumer.get('/me').track_count
+	end
+	def soundcloud_collections
+			$sc_consumer.get('/me').playlist_count
+	end
 end
