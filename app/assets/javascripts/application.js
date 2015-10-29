4a041153845fca2d8fc145376efa1761db8cd9ad
klaoha06 <laohasongkram.supakorn@gmail.com>
2015-10-29 21:11:43 +0700

    fix compiling assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,64 @@
 //= require wow.min
 //= require creative
 //= require filterrific/filterrific-jquery
+//= require imagesloaded.pkgd.min
+//= require isotope.pkgd.min
 //= require_self
 //= require google_analytics
 //= stub active_admin/application
 //= stub ckeditor/init
 //= stub activeadmin
+
+new WOW().init();
+// Start Owl Carousel
+$("#owl-books").owlCarousel({
+	navigation : true,
+	autoPlay : true,
+	stopOnHover : true,
+	scrollPerPage: true,
+  slideSpeed: 900,
+  items: 4,
+  navigationText: [
+    "<i class='fa fa-angle-left fa-4x'></i>",
+    "<i class='fa fa-angle-right fa-4x'></i>",
+    ],
+});
+
+// $("#owl-articles").owlCarousel({
+// 	nav : true,
+// 	autoPlay : true,
+// 	stopOnHover : true,
+// 	scrollPerPage: true,
+//     slideSpeed: 900
+// });
+
+$("#owl-poetry").owlCarousel({
+    items: 1,
+    navigation:true,
+    // autoPlay : true,
+    // animateOut: 'fadeOut',
+    // animateIn: 'fadeIn',
+    // loop: true,
+    navigationText: [
+    "<i class='fa fa-angle-left fa-4x white'></i>",
+    "<i class='fa fa-angle-right fa-4x white'></i>",
+      ],
+    transitionStyle : "fade",
+    autoHeight:true
+});
+
+
+var $grid = $('.grid').isotope({
+  // options...
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    // use element for option
+    columnWidth: '.grid-sizer'
+  }
+  // layoutMode: 'packery'
+});
+// layout Isotope after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.isotope('layout');
+});
