9b1d5d244acceb67ae403d06998ccbab7f77faf2
klaoha06 <laohasongkram.supakorn@gmail.com>
2015-10-29 21:57:39 +0700

    fix js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,8 @@
 //= require imagesloaded.pkgd.min
 //= require isotope.pkgd.min
 //= require_self
+//= require books
+//= require poems
 //= require google_analytics
 //= stub active_admin/application
 //= stub ckeditor/init
@@ -42,14 +44,6 @@ $("#owl-books").owlCarousel({
     ],
 });
 
-// $("#owl-articles").owlCarousel({
-// 	nav : true,
-// 	autoPlay : true,
-// 	stopOnHover : true,
-// 	scrollPerPage: true,
-//     slideSpeed: 900
-// });
-
 $("#owl-poetry").owlCarousel({
     items: 1,
     navigation:true,
@@ -57,6 +51,11 @@ $("#owl-poetry").owlCarousel({
     // animateOut: 'fadeOut',
     // animateIn: 'fadeIn',
     // loop: true,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsTabletSmall: false,
+    itemsMobile: false,
     navigationText: [
     "<i class='fa fa-angle-left fa-4x white'></i>",
     "<i class='fa fa-angle-right fa-4x white'></i>",
@@ -64,7 +63,6 @@ $("#owl-poetry").owlCarousel({
     transitionStyle : "fade",
     autoHeight:true
 });
-
 
 var $grid = $('.grid').isotope({
   // options...
@@ -79,4 +77,45 @@ var $grid = $('.grid').isotope({
 // layout Isotope after each image loads
 $grid.imagesLoaded().progress( function() {
   $grid.isotope('layout');
+});
+
+  var style = document.documentElement.style;
+  support3d = (style.webkitTransition !== undefined ||
+               style.MozTransition !== undefined ||
+               style.OTransition !== undefined ||
+               style.transition !== undefined);
+  var owl = $("#owl-featured-retreat_talks");
+  $('#owl-recommended-retreat_talks').owlCarousel({
+      items: 5,
+  });
+
+$("#owl-featured-retreat_talks").owlCarousel({
+    items: 1,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsTabletSmall: false,
+    itemsMobile: false,
+    autoHeight:true
+});  
+ $("#owl_related_retreat_talks").owlCarousel({  
+  items : 4,
+  itemsDesktop : [1199,3],
+  itemsDesktopSmall : [979,3]
+});
+
+ var $poemGrid = $('.poem-grid').isotope({
+  // options...
+  itemSelector: '.poem-grid-item',
+  percentPosition: true,
+  masonry: {
+    // use element for option
+    columnWidth: '.poem-grid-sizer',
+    gutter: 10
+  }
+  // layoutMode: 'packery'
+});
+// layout Isotope after each image loads
+$poemGrid.imagesLoaded().progress( function() {
+  $poemGrid.isotope('layout');
 });
--- app/assets/javascripts/poems.js
+++ app/assets/javascripts/poems.js
@@ -1,17 +1,3 @@
-var $poemGrid = $('.poem-grid').isotope({
-  // options...
-  itemSelector: '.poem-grid-item',
-  percentPosition: true,
-  masonry: {
-    // use element for option
-    columnWidth: '.poem-grid-sizer',
-    gutter: 10
-  }
-  // layoutMode: 'packery'
-});
-// layout Isotope after each image loads
-$poemGrid.imagesLoaded().progress( function() {
-  $poemGrid.isotope('layout');
-});
+
 
 // debugger;
