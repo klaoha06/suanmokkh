7fc826a2932cc35de9be55b68f0b251a54348d3d
klaoha06 <laohasongkram.supakorn@gmail.com>
2015-12-14 21:33:18 +0700

    revised

--- app/models/book.rb
+++ app/models/book.rb
@@ -121,7 +121,7 @@ class Book < ActiveRecord::Base
     if !self.external_file_link.blank? 
       return '<iframe src="' 'https://drive.google.com/viewerng/viewer?embedded=true&url=' + self.external_file_link + '#page=1&zoom=100' + '"#view=fit" width="100%" height="1000px" border="0" style="border:none" scrolling="no"></iframe>'
     elsif !self.file.url.blank? && !self.file.url.include?('missing')
-      return '<iframe src="' + self.file.url + '#page=1&zoom=100' + '"#view=fitH" width="100%" height="1000px" border="0" style="border:none" scrolling="no"></iframe>'
+      return '<iframe src="' 'https://drive.google.com/viewerng/viewer?embedded=true&url=' + self.file.url + '#page=1&zoom=100' + '"#view=fitH" width="100%" height="1000px" border="0" style="border:none" scrolling="no"></iframe>'
     else
       return false
     end 
