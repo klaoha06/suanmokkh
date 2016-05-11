0f18d1da708d2c94bd9a85a36596815f4a37bb79
klaoha06 <laohasongkram.supakorn@gmail.com>
2016-05-11 16:53:37 +0700

    remove spree for now

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
+  
+  #spree
   # mount Spree::Core::Engine, :at => '/store'
+  #spree
+  
   resources :feedbacks, :except => [:update, :destroy, :index, :show]
   resources :collections, :except => [:update, :destroy, :index, :show]
   # resources :relationships
