88a4bd1bd8aaa8c77ccd7324b1fbb25620160ca6
klaoha06 <laohasongkram.supakorn@gmail.com>
2017-12-04 13:17:36 +0700

    fix grid

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -103,42 +103,42 @@ $("#owl-featured-retreat_talks").owlCarousel({
 //   }
 // });
 
- var columns;
+ // var columns;
 
- // set column number
- setColumns();
+ // // set column number
+ // setColumns();
 
- // rerun function when window is resized 
- $(window).on('resize', function() {
-   setColumns();
- });
+ // // rerun function when window is resized 
+ // $(window).on('resize', function() {
+ //   setColumns();
+ // });
 
- // the function to decide the number of columns
- function setColumns() {
-   if($(window).width() <= 750) {
-    $( window ).on("resize", function(){
-        // Change the width of the div
-            $('.poem-grid-sizer').css("width", "100%");
-            $('.poem-grid-item').css("width", "100%");
-    });
-   } 
+ // // the function to decide the number of columns
+ // function setColumns() {
+ //   if($(window).width() <= 750) {
+ //    $( window ).on("resize", function(){
+ //        // Change the width of the div
+ //            $('.poem-grid-sizer').css("width", "100%");
+ //            $('.poem-grid-item').css("width", "100%");
+ //    });
+ //   } 
 
-  if($(window).width() > 750) {
-     var $container = $('.poem-grid');
-     // initialize Isotope
-     $container.isotope({
-       // options...
-       resizable: false, // disable normal resizing
-       // set columnWidth to a percentage of container width
-       masonry: { columnWidth: $container.width() / 3 }
-     });
-     $( window ).on("resize", function(){
-         // Change the width of the div
-             $('.poem-grid-sizer').css("width", "33%");
-             $('.poem-grid-item').css("width", "33%");
-     });
-   }
- }
+ //  if($(window).width() > 750) {
+ //     var $container = $('.poem-grid');
+ //     // initialize Isotope
+ //     $container.isotope({
+ //       // options...
+ //       resizable: false, // disable normal resizing
+ //       // set columnWidth to a percentage of container width
+ //       masonry: { columnWidth: $container.width() / 3 }
+ //     });
+ //     $( window ).on("resize", function(){
+ //         // Change the width of the div
+ //             $('.poem-grid-sizer').css("width", "33%");
+ //             $('.poem-grid-item').css("width", "33%");
+ //     });
+ //   }
+ // }
 
 // layout Isotope after each image loads
 // $poemGrid.imagesLoaded().progress( function() {
--- app/assets/javascripts/poems.js
+++ app/assets/javascripts/poems.js
@@ -1,3 +1,36 @@
+ var columns;
 
+ // set column number
+ setColumns();
 
-// debugger;
+ // rerun function when window is resized 
+ $(window).on('resize', function() {
+   setColumns();
+ });
+
+ // the function to decide the number of columns
+ function setColumns() {
+   if($(window).width() <= 750) {
+    $( window ).on("resize", function(){
+        // Change the width of the div
+            $('.poem-grid-sizer').css("width", "100%");
+            $('.poem-grid-item').css("width", "100%");
+    });
+   } 
+
+  if($(window).width() > 750) {
+     var $container = $('.poem-grid');
+     // initialize Isotope
+     $container.isotope({
+       // options...
+       resizable: false, // disable normal resizing
+       // set columnWidth to a percentage of container width
+       masonry: { columnWidth: $container.width() / 3 }
+     });
+     $( window ).on("resize", function(){
+         // Change the width of the div
+             $('.poem-grid-sizer').css("width", "33%");
+             $('.poem-grid-item').css("width", "33%");
+     });
+   }
+ }
