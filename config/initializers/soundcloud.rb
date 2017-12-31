267818e145fc35d2dd6ec7fab76635bc150276f9
klaoha06 <laohasongkram.supakorn@gmail.com>
2017-12-31 21:49:42 +0700

    yo

--- config/initializers/soundcloud.rb
+++ config/initializers/soundcloud.rb
@@ -3,10 +3,10 @@ require 'rufus-scheduler'
 
 Rails.application.config.after_initialize do
   # create client object with app and user credentials
-  $sc_consumer = Soundcloud.new(:client_id => Rails.application.secrets.SC_CLIENT_ID,
-                          :client_secret => Rails.application.secrets.SC_CLIENT_SECRET,
-                          :username => Rails.application.secrets.SC_USERNAME,
-                          :password => Rails.application.secrets.SC_PASSWORD)
+  # $sc_consumer = Soundcloud.new(:client_id => Rails.application.secrets.SC_CLIENT_ID,
+  #                         :client_secret => Rails.application.secrets.SC_CLIENT_SECRET,
+  #                         :username => Rails.application.secrets.SC_USERNAME,
+  #                         :password => Rails.application.secrets.SC_PASSWORD)
 
   # scheduler = Rufus::Scheduler.new
 
